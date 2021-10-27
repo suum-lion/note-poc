1a1ce6a31cc192d2998842d81ef02ba20defd69a
suum-lion <suum@likelion.net>
2021-10-27 16:44:35 +0900

    perf: ⚡️ change placeholder and button text

--- src/presentation/components/Auth/SignInWithEmailForm.tsx
+++ src/presentation/components/Auth/SignInWithEmailForm.tsx
@@ -25,7 +25,7 @@ const SignInWithEmailForm: FC<ISignInWithEmailFormProps> = ({
         <input
           name="email"
           type="email"
-          placeholder="Email"
+          placeholder="이메일"
           required
           value={email}
           onChange={inputFormChangeHandler}
@@ -33,16 +33,18 @@ const SignInWithEmailForm: FC<ISignInWithEmailFormProps> = ({
         <input
           name="password"
           type="password"
-          placeholder="Password"
+          placeholder="비밀번호"
           required
           value={password}
           onChange={inputFormChangeHandler}
         />
-        <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
+        <input type="submit" value={newAccount ? '회원가입' : '로그인'} />
         {error ? <div>{error}</div> : null}
       </form>
       <div>
-        <button onClick={toggleAccount}>{newAccount ? 'Sign In' : 'Create Account'}</button>
+        <button type="button" data-testid="toggleButton" onClick={toggleAccount}>
+          {newAccount ? '로그인하기' : '회원가입하기'}
+        </button>
       </div>
     </>
   )
